7b2941932adb6ae258a315e832b88e5a2938ea67
Marco Willian Bezerra Ferreira <marco.ferreira@enext.com.br>
2021-03-10 23:30:05 -0300

    feat(general): add struct and files step

---
+++ src/router/AppRouter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import FirstStep from '../components/FirstStep';
+import SecondStep from '../components/SecondStep';
+import ThirdStep from '../components/ThirdStep';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route component={FirstStep} path='/' exact={true} />
+        <Route component={SecondStep} path="/second" />
+        <Route component={ThirdStep} path="/third" />
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
+
+export default AppRouter;
