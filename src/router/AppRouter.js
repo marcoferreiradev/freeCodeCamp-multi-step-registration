a4659a0219525bcb8f62d8410cc5a4b131109121
Marco Willian Bezerra Ferreira <marco.ferreira@enext.com.br>
2021-03-12 00:41:50 -0300

    feat(steps): persisting data between routes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import FirstStep from '../components/FirstStep';
 import SecondStep from '../components/SecondStep';
 import ThirdStep from '../components/ThirdStep';
 
-const AppRouter = () => (
-  <BrowserRouter>
-    <div className="container">
-      <Header />
-      <Switch>
-        <Route component={FirstStep} path='/' exact={true} />
-        <Route component={SecondStep} path="/second" />
-        <Route component={ThirdStep} path="/third" />
-      </Switch>
-    </div>
-  </BrowserRouter>
-)
+const AppRouter = () => {
+  const [user, setUser] = useState({});
+
+  const updateUser = (data) => {
+    setUser((prevUser) => ({ ...prevUser, ...data }));
+  };
+
+  const resetUser = () => {
+    setUser({});
+  };
+
+  return(
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route
+            render={(props) => (
+              <FirstStep {...props} user={user} updateUser={updateUser} />
+            )}
+            path="/"
+            exact={true}
+          />
+          <Route
+            render={(props) => (
+              <SecondStep {...props} user={user} updateUser={updateUser} />
+            )}
+            path="/second"
+          />
+          <Route
+            render={(props) => (
+              <ThirdStep {...props} user={user}  />
+            )}
+            path="/third"
+          />
+        </Switch>
+      </div>
+    </BrowserRouter>
+
+  )
+}
 
 export default AppRouter;
