0346dc8361596cd1bb8e99f162c505652c731cb6
Marco Willian Bezerra Ferreira <marco.ferreira@enext.com.br>
2021-03-12 20:49:42 -0300

    feat(AppRouter): redirect to path not defined

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import FirstStep from '../components/FirstStep';
 import SecondStep from '../components/SecondStep';
@@ -42,6 +42,7 @@ const AppRouter = () => {
             path="/third"
           />
           <Route component={Login} path="/login" />
+          <Route render={() => <Redirect to="/" />} />
         </Switch>
       </div>
     </BrowserRouter>
